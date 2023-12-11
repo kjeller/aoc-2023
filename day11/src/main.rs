d950b5452d8ceea86a3b42966b8b585b07aecc60
kjeller <kallestralman@gmail.com>
2023-12-11 13:40:49 +0100

    Day 11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -3,105 +3,86 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-use pathfinding::directed::dijkstra::dijkstra;
-
-const SIZE: usize = 140;
-
 #[derive(Debug)]
 struct Image {
-    start: Vec<(isize, isize)>,
+    galaxies: Vec<(u128, u128)>,
 }
 
 impl Image {
     fn new() -> Image {
-        Image { start: Vec::new() }
-    }
-
-    fn get_valid_paths(&mut self, (x, y): (isize, isize)) -> [((isize, isize), isize); 4] {
-        let weight = 1;
-        let paths: [((isize, isize), isize); 4] = [
-            ((x - 1, y), weight),
-            ((x + 1, y), weight),
-            ((x, y - 1), weight),
-            ((x, y + 1), weight),
-        ];
-        paths
+        Image {
+            galaxies: Vec::new(),
+        }
     }
 }
 
-fn parse() -> Image {
+impl Image {
+    fn sum_distances(&self) -> u128 {
+        self.galaxies
+            .iter()
+            .enumerate()
+            .map(|(i, s1)| {
+                self.galaxies[i..self.galaxies.len()]
+                    .iter()
+                    .map(|s2| {
+                        if s1 != s2 {
+                            let res = s1.0.abs_diff(s2.0) + s1.1.abs_diff(s2.1);
+                            res
+                        } else {
+                            0
+                        }
+                    })
+                    .sum::<u128>()
+            })
+            .sum()
+    }
+}
+
+fn parse(expand_size: u128) -> Image {
     let file = File::open("input.txt").expect("not found");
     let reader = BufReader::new(file);
     let mut image = Image::new();
+    let width = 140;
     reader
         .lines()
         .map(Result::unwrap)
         .enumerate()
         .for_each(|(row, line)| {
             line.chars().enumerate().for_each(|(col, c)| match c {
-                '#' => image.start.push((col as isize, row as isize)),
+                '#' => image.galaxies.push((col as u128, row as u128)),
                 _ => (),
             });
         });
+    let mut start = image.galaxies.clone();
+    let expand_y = (0..width)
+        .into_iter()
+        .filter(|f| !start.iter().map(|f| f.1).any(|s| *f == s))
+        .collect::<Vec<u128>>();
+
+    expand_y.iter().rev().for_each(|f| {
+        start.iter_mut().filter(|s| s.1 > *f).for_each(|s| {
+            s.1 += expand_size;
+        });
+    });
+    let expand_x = (0..width)
+        .into_iter()
+        .filter(|f| !start.iter().map(|f| f.0).any(|s| *f == s))
+        .collect::<Vec<u128>>();
+    expand_x.iter().rev().for_each(|f| {
+        start.iter_mut().filter(|s| s.0 > *f as u128).for_each(|s| {
+            s.0 += expand_size;
+        });
+    });
+    image.galaxies = start;
     image
 }
 
-fn part_1() -> u64 {
-    let mut image = parse();
-    let mut start = image.start.clone();
-
-    let expand_y = (0..SIZE)
-        .into_iter()
-        .filter(|f| !start.iter().map(|f| f.1).any(|s| *f as isize == s))
-        .collect::<Vec<usize>>();
-
-    expand_y.iter().rev().for_each(|f| {
-        start
-            .iter_mut()
-            .filter(|s| s.1 > *f as isize)
-            .for_each(|s| {
-                s.1 += 1;
-            });
-    });
-    let expand_x = (0..SIZE)
-        .into_iter()
-        .filter(|f| !start.iter().map(|f| f.0).any(|s| *f as isize == s))
-        .collect::<Vec<usize>>();
-
-    expand_x.iter().rev().for_each(|f| {
-        start
-            .iter_mut()
-            .filter(|s| s.0 > *f as isize)
-            .for_each(|s| {
-                s.0 += 1;
-            });
-    });
-
-    start
-        .iter()
-        .enumerate()
-        .map(|(i, s1)| {
-            start[i..start.len()]
-                .iter()
-                .map(|s2| {
-                    if s1 != s2 {
-                        let res = dijkstra(s1, |p| image.get_valid_paths(*p), |p| *p == *s2);
-                        let res = match res {
-                            Some(x) => x.1 as u64,
-                            None => 0,
-                        };
-                        res
-                    } else {
-                        0
-                    }
-                })
-                .sum::<u64>()
-        })
-        .sum()
+fn part_1() -> u128 {
+    parse(1).sum_distances()
 }
 
-fn part_2() -> i64 {
-    0
+fn part_2() -> u128 {
+    parse(999_999).sum_distances()
 }
 
 fn main() {
